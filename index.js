a8ee5f9e1f4b8d01543534547ac61c07556a5826
Rolando Murillo <murillorafael7@gmail.com>
2021-08-27 22:17:34 -0400

    views and more routes

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+let express = require('express')
+let path = require('path')
+let app = express()
+let port = 3000
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.render('index', {foo: 'FOO'});
+});
+
+app.get('/beginner_topics/part_1', (req, res) => {
+  res.render('beginner_topics/part_1');
 })
 
 app.get('/assets/css/mediumish.css', (req, res) => {
@@ -15,6 +21,16 @@ app.get('/assets/css/bootstrap.min.css', (req, res) => {
   res.sendFile(path.join(__dirname, '/assets/css/bootstrap.min.css'))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/assets/img/*', (req, res) => {
+  res.sendFile(path.join(__dirname, req.path))
 })
+
+app.get('/assets/js/*', (req, res) => {
+  res.sendFile(path.join(__dirname, req.path))
+})
+
+app.get('/assets/css/*', (req, res) => {
+  res.sendFile(path.join(__dirname, req.path))
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
